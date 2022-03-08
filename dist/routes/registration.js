d2eb0b667a0e08a8ece977f3ba32707156411067
jagannath-mobifyi <jagannath@mobifyi.com>
2022-03-08 16:02:48 +0530

    make it better

--- dist/routes/registration.js
+++ dist/routes/registration.js
@@ -4,9 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const BasicAuth_1 = require("../middlewares/BasicAuth");
 const router = express_1.default.Router();
 const users_1 = require("../controllers/users");
-router.post("/", BasicAuth_1.BasicAuth, users_1.registration);
+router.post("/", users_1.registration);
 exports.default = router;
 //# sourceMappingURL=registration.js.map
